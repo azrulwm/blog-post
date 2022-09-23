7010731e61505f7442d1d1a85df9cc62b6803ca8
azrulwm <azrulazmi.wm@gmail.com>
2022-09-23 20:42:37 +0800

    uploading ejs files

--- app.js
+++ app.js
@@ -5,23 +5,39 @@ const express = require("express");
 // express app
 const app = express();
 
+// register view engine
+app.set("view engine", "ejs");
+
 // listen for requests
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+    {
+      title: "Mario finds star",
+      snippet: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+    {
+      title: "How to defeat Bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+  ];
+  res.render("index", { title: "Home", blogs: blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "About" });
 });
 
-// redirects
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a new Blog" });
 });
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: "404" });
 });
